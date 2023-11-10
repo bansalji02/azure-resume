ca2ff52a6a2b0212cac7e2f15d9de1d704333009
bansalji02 <hb13april2002@gmail.com>
2023-11-10 13:08:37 +0530

    Changed the complete frontend

--- frontend/main.js
+++ frontend/main.js
@@ -22,10 +22,3 @@ const getVisitCount = () =>{
     
 }
 
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
-  
